388927746a07f8ca2338e2d652a556d8728e4814
uniksneha <uniksneha@gmail.com>
2025-05-04 12:20:41 -0400

    url changed

--- Program.cs
+++ Program.cs
@@ -158,7 +158,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowAngularClient", policyBuilder =>
     {
         policyBuilder
-            .WithOrigins("https://pizzaorderclient-b2ddeaduhfcvereu.canadacentral-01.azurewebsites.net", "http://localhost:4200")
+            .WithOrigins("https://pizzaorderapi-buh3dvavakbbb7df.canadacentral-01.azurewebsites.net", "http://localhost:4200")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
